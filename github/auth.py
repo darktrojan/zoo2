c82d653ed7f17cd58f0451f98a80aa3b2ec302eb
Geoff Lankow <geoff@darktrojan.net>
2015-08-01 13:39:33 +1200

    OAuth endpoint does not use JSON

--- github/auth.py
+++ github/auth.py
@@ -6,13 +6,9 @@ _headers = dict()
 _headers['Accept'] = 'application/json'
 
 def _do_thing(method, path, body=None, is_retry=False):
-	body_json = None
-	if body is not None:
-		body_json = json.dumps(body)
-
 	try:
 		print path
-		_api_conn.request(method, path, headers=_headers, body=body_json)
+		_api_conn.request(method, path, headers=_headers, body=body)
 		response = _api_conn.getresponse()
 		print response.status
 		response_body = response.read()
@@ -29,10 +25,8 @@ def _do_thing(method, path, body=None, is_retry=False):
 		return _do_thing(method, path, body, is_retry=True)
 
 def get_access_token(code, state):
-	body = {
-		'client_id': '7d5bd4b7b4ee040275ba',
-		'client_secret': os.environ['GITHUB_CLIENT_SECRET'],
-		'code': code,
-		'state': state
-	}
-	return _do_thing('POST', '/login/oauth/access_token')
+	body = 'client_id=%s&client_secret=%s&code=%s' % (
+		'7d5bd4b7b4ee040275ba', os.environ['GITHUB_CLIENT_SECRET'], code
+	)
+	print body
+	return _do_thing('POST', '/login/oauth/access_token', body)
