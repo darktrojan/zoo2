3cd7350a4c94fb5b1c91fce1d60fa2011d32b7f6
Geoff Lankow <geoff@darktrojan.net>
2015-09-19 11:36:56 +1200

    Stop using a shared HTTPS connection for raw file requests

--- github/raw.py
+++ github/raw.py
@@ -1,24 +1,16 @@
 import os.path
-from httplib import BadStatusLine, HTTPSConnection
-
-_raw_conn = HTTPSConnection('raw.githubusercontent.com')
+from httplib import HTTPSConnection
 
 
-def get_raw_file(repo, commit_sha, path, is_retry=False):
+def get_raw_file(repo, commit_sha, path):
+	raw_conn = HTTPSConnection('raw.githubusercontent.com')
 	path = os.path.join('/', repo, commit_sha, path)
-	try:
-		print ' '.join(['GET', path])
-		_raw_conn.request('GET', path)
-		res = _raw_conn.getresponse()
-		print ' '.join(['GET', path, str(res.status)])
-		response_body = res.read()
-		if res.status == 404:
-			return None
-		return response_body
-	except BadStatusLine as ex:
-		if is_retry:
-			raise ex
 
-		print 'closing connection and trying again'
-		_raw_conn.close()
-		return get_raw_file(repo, commit_sha, path, is_retry=True)
+	print ' '.join(['GET', path])
+	raw_conn.request('GET', path)
+	res = raw_conn.getresponse()
+	print ' '.join(['GET', path, str(res.status)])
+	response_body = res.read()
+	if res.status == 404:
+		return None
+	return response_body
